890d75bc0a27674da1f9c9751320eb3570dccf94
Artur Filho <artureng7@gmail.com>
2020-05-23 20:27:21 -0300

    ex 1 lista 1

---
+++ ex_1_lista1.js
@@ -0,0 +1,14 @@
+//Escreva um programa para ler o ano de nascimento de uma pessoa e
+//escrever uma mensagem que diga se ela poderá ou não votar este ano (não é
+//necessário considerar o mês em que ela nasceu).
+
+var rs = require('readline-sync')
+var ano = rs.questionInt('Digite o ano do nascimento: ')
+
+var limite = 2002
+
+if(ano >= 2002) {
+    console.log('Você poderá votar neste ano.')
+} else {
+    console.log('Você não poderá votar neste ano.')
+}
