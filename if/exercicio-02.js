bec772395389080515e8515b3f1bfb8719fa0072
Artur Filho <artureng7@gmail.com>
2020-06-04 08:31:12 -0300

    upando exercicios

--- if/exercicio-02.js
+++ if/exercicio-02.js
@@ -1,8 +1,6 @@
 //Faça um programa que receba um número do usuário e verifique se
 //este número é par ou ímpar, escrevendo na tela “PAR” ou “ÍMPAR”
 
-var rs = require('readline-sync')
-var numero = rs.questionInt('Digite o numero: ')
 
 if(numero % 2 == 0) {
     console.log('o número é PAR')
