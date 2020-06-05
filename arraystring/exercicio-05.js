0f2bb00056ca2a51f9444e4a4af428bf3f386a6a
Artur Filho <artureng7@gmail.com>
2020-06-05 10:48:27 -0300

    receber nome de dois jogadores, pedir 5 números de 1 a 10 para o joga…
    …dor A e depois para o jogador B. Mostrar os números escolhidos pelos dois jogadores e imprimir quais foram os números que apenas o jogador B colocou diferente do A

---
+++ arraystring/exercicio-05.js
@@ -0,0 +1,44 @@
+// Faça um algoritmo que receba o nome de dois jogadores e após isso peça
+// 5 números de 1 a 10 para cada jogador. Primeiro para o jogador A e depois para o
+// jogador B.
+// Após a escolha de cada jogador o algoritmo deve apresentar quais foram os números
+// que apenas o jogador A e B colocou sem repetir nenhum número
+// EX: Jogador A : 1, 2, 3, 4, 5
+// Jogador B : 1,2,3,8,7
+// Algoritmo imprime :
+// 1,2,3,4,5,7,8
+
+var rs = require('readline-sync')
+var jogador1, jogador2, numeroA, numeroB, diferentes
+jogador1 = rs.question('Digite o nome do primeiro jogador: ')
+jogador2 = rs.question('Digite o nome do segundo jogador: ')
+numeroA = []
+numeroB = []
+iguais = []
+
+for (var i = 0; i < 5; i++) {
+    var num1 = rs.questionInt(jogador1 + ' Digite um numero de 1 a 10: ') 
+    numeroA.push(num1) //número que vai entrar no array
+}
+
+for (var k = 0; k < 5; k++) {
+    var num2 = rs.questionInt(jogador2 + ' Digite um numero de 1 a 10: ')
+    numeroB.push(num2)  //número que vai entrar no array
+}
+console.log(jogador1 + ': ' + numeroA)
+console.log(jogador2 + ': ' + numeroB)
+
+
+    for (var j = 0; j < numeroB.length; j++) {
+        numeroA.toString() //transforma o array de números em string
+        iguais = numeroA
+        
+        if(numeroA.includes(numeroB[j])) { //includes verifica se a string (numeroB) contém algum número da array numeroA
+
+        } else {
+            iguais.push(numeroB[j])
+        }
+        
+    }
+
+console.log(iguais)
