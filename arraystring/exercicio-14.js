ff0acc37368781e7ea1a1fc14a7e8c3c0a36fc54
Artur Filho <artureng7@gmail.com>
2020-06-06 21:50:07 -0300

    receber uma string em formato de data e imprimir o dia, mês e ano separadamente

---
+++ arraystring/exercicio-14.js
@@ -0,0 +1,29 @@
+// Faça um programa que receba uma string, o programa vai estar esperando uma data no
+// formato dd/MM/yyyy onde :
+// a. dd -> dia
+// b. mm -> mês
+// c. yyyy -> ano
+// Utilize o método match() para fazer essa validação, você deverá utilizar um expressão
+// regular para resolver este problema.
+// Caso a data esteja valida o algoritmo deve imprimir :
+// Entrada -> 22/05/2020
+// O algoritmo imprime :
+// Dia : 22
+// Mes : 05
+// Ano : 2020
+
+var rs = require('readline-sync')
+var data = rs.question('Digite uma data no formato 00/00/0000: ')
+
+var dia = data.match(/(\d\d)/)
+console.log('Dia: ' + dia[0])
+data = data.substring(dia['index'] + 3)
+
+var mes = data.match(/(\d\d)/)
+console.log('Mes: ' + mes[0])
+data = data.substring(mes['index'] + 3)
+
+var ano = data.match(/(\d\d\d\d)/)
+console.log('Ano: ' + ano[0])
+data = data.substring(ano['index'] + 3)
+
