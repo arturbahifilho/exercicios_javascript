360c852b68638a8a171fb97b2c72e60e25d6238c
Artur Filho <artureng7@gmail.com>
2020-06-10 02:14:17 -0300

    ler arquivo carro.json utilizando a biblioteca 'fs' e desserializar o objeto em string, após isso imprimir os valores lidos do json

--- objects/exercicio-03.js
+++ objects/exercicio-03.js
@@ -4,16 +4,16 @@
 // OBS: altere o arquivo salve e rode o programa novamente e veja os dados sendo
 // mudados a cada execução.
 
-var rs=require('readline-sync')
-var fs=require('fs')
-var jsons=fs.readFileSync('carro.json')
-var carro=JSON.parse(jsons)
+// var rs=require('readline-sync')
+// var fs=require('fs')
+// var jsons=fs.readFileSync('carro.json')
+// var carro=JSON.parse(jsons)
 
-carro.cor=rs.question('Digite a cor: ')
-carro.modelo=rs.question('Digite o modelo: ')
-carro.marca=rs.question('Digite a marca: ')
-console.log(carro)
+// carro.cor=rs.question('Digite a cor: ')
+// carro.modelo=rs.question('Digite o modelo: ')
+// carro.marca=rs.question('Digite a marca: ')
+// console.log(carro)
 
-var carroSerializado= JSON.stringify(carro)
+// var carroSerializado= JSON.stringify(carro)
 
-fs.writeFileSync('carro.json',carroSerializado)
+// fs.writeFileSync('carro.json',carroSerializado)
