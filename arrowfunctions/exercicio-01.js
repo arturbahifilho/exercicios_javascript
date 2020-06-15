5c9034d8936ccdf6df7c6269bae5d8eee88d0ef2
Artur Filho <artureng7@gmail.com>
2020-06-14 21:04:37 -0300

    solicitar um número de potência e imprimir quantos carros tem a potência maior ou igual ao valor informado

---
+++ arrowfunctions/exercicio-01.js
@@ -0,0 +1,16 @@
+// Crie um algoritmo que solicite um número para o usuário e diga quantos carros mais
+// potente que o valor informado.
+// Para isso utiliza a propriedade “Horserpower” do cars.json
+// DICA: função filter ou procure como contar quantos objetos tem x valor em uma lista no
+// google.
+
+var rs = require('readline-sync')
+var carros = require('../arrowfunctions/cars.json')
+var hp = rs.questionInt('Qual o minimo HP dos carros? ')
+
+var carrosComHpMinimo = carros.filter(
+
+    (carro) => carro.Horsepower >= hp)
+    
+    console.log(carrosComHpMinimo.length + ' carro(s)')
+    
