722c028a556eaf82db9796d5b85722741a6dcf4d
Artur Bahi Filho <artureng7@gmail.com>
2020-06-24 23:50:26 -0300

    Tarefa básica

--- Desafios/Pokedex/poke-api.js
+++ Desafios/Pokedex/poke-api.js
@@ -12,36 +12,47 @@ var url = 'https://pokeapi.co/api/v2'
 const axios = require('axios')
 const rs = require('readline-sync')
 var fs = require('fs')
+const { info } = require('console')
 
-var filtro = rs.question('Digite o nome ou numero de um pokemon: ')
+var filtro = rs.question('Digite o nome ou numero de um pokemon: ').toLocaleLowerCase()
 
 //console.log(pesquisa)
 
 axios.get(`${url}/pokemon/${filtro}`)
     .then((resposta) => {
-        
+
         const pokemon = resposta.data
-        console.log('Nome: ', pokemon.name)
+        var infoPokemon = { nome: '', tipo: [], habilidades: [] }
+        var pokemons = []
+      
+        infoPokemon.nome = pokemon.name
 
-        var nomeDosTipos = []
-        pokemon.types.forEach((tipo) => { //forEach => usado em array
-            nomeDosTipos.push(tipo.type.name)
+
+        pokemon.types.forEach((element) => { //forEach => usado em array
+            infoPokemon.tipo.push(element.type.name)
         })
-        console.log('Tipo: ', nomeDosTipos)
 
-        var nomeDasHabilidades = []
+        pokemon.abilities.forEach((element) => {
+            infoPokemon.habilidades.push(element.ability.name)
 
-        pokemon.abilities.forEach((habilidade) => {
-            nomeDasHabilidades.push(habilidade.ability.name)
-            
         })
-        console.log('Habilidade: ', nomeDasHabilidades)
+        console.log(infoPokemon)
+        pokemons.push(infoPokemon)
 
-    pokemonSerializado = JSON.stringify(pokemon)
-    fs.writeFileSync('pokemons.json', pokemonSerializado)
+        var pergunta = rs.question('Voce deseja salvar o pokemon? Sim / Nao: ')
+        if(pergunta == 'Sim' || pergunta == 'sim' || pergunta == 's'){
+            pokemonSerializado = JSON.stringify(pokemons)
+            fs.writeFileSync('pokemons.json', pokemonSerializado)
+            console.log('Pokemon salvo!')
+        } else if(pergunta == 'Nao' || pergunta == 'nao' || pergunta == 'n') {
+            console.log('Tudo bem!')
+        } else {
+            console.log('Digite apenas Sim ou Nao')
+        }
 
     }).catch((error) => {
         console.log(error)
     })
 
-    
+   
+
