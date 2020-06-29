3996de73d95bc6358667554ff96dac0a4c689d71
Artur Bahi Filho <artureng7@gmail.com>
2020-06-29 18:36:23 -0300

    parte intermediaria feita

--- Desafios/Pokeapi/pokeapi.js
+++ Desafios/Pokeapi/pokeapi.js
@@ -5,7 +5,7 @@ var fs = require('fs')
 var filtro = rs.question('Digite o nome ou numero de um pokemon: ').toLocaleLowerCase()
 
 axios.get(`https://pokeapi.co/api/v2/pokemon/${filtro}`)
-// function trataRespostaApi(resp) {
+
     .then((resp) => {
     let dados = resp.data
     var infoPokemon = {nome: '', id: '',tipo: [], habilidade: []}
@@ -20,19 +20,7 @@ axios.get(`https://pokeapi.co/api/v2/pokemon/${filtro}`)
             infoPokemon.habilidade.push(element.ability.name)
         })
 
-        // axios.get(`https://pokeapi.co/api/v2/ability/${infoPokemon.habilidade}`)
-        // var pokemonsSalvos = fs.readFileSync('Desafios/Pokeapi/pokemons.json')
-        // var pokemons = JSON.parse(pokemonsSalvos)
-    
-        // var pokemonSerializado = JSON.stringify(pokemons)
-        // fs.writeFileSync('Desafios/Pokedex/pokemons.json', pokemonSerializado)
-        // var infoHabilidade = {efeito: []}
-        // var habilidade = []
-        // dados.forms.forEach((element) => {
-        // infoHabilidade.efeito.push(element.name)
-        // habilidade.push(infoHabilidade)
-        // console.log(habilidade)
-        // })
+       
 
     pokemons.push(infoPokemon)
     console.log(pokemons)
@@ -72,12 +60,7 @@ axios.get(`https://pokeapi.co/api/v2/pokemon/${filtro}`)
                     habilidade.push(infoHabilidade)
                     console.log(habilidade)
                 }
-            //     dadosHab.effect_entries.forEach((element) => {
-            //     infoHabilidade.efeito.push(element.effect)
-            //     habilidade.push(infoHabilidade)
-            //     console.log(habilidade)
-        
-            // })
+           
         }).catch((error) => {
             console.log(error)
         })
@@ -93,12 +76,11 @@ var pokemonsSalvos = fs.readFileSync('Desafios/Pokeapi/pokemons.json')
         axios.get(`https://pokeapi.co/api/v2/type/${tipo}`)
         .then((respo) => {
             let dadosTipo = respo.data
-            var infoDano = {dobroDoDano: [], metadeDoDano: [], nenhumDano: []}
-            var infoDano2 = {}
-            var infoDano3 = {}
+            var infoDano = {dobroDoDano: [], metadeDoDano: [], nenhumDano: [], mesmoTipo: []}
             var danosPokemon = []
             var danosPokemon2 = []
             var danosPokemon3 = []
+            var tiposPokemon = []
                 for(var i = 0; i < dadosTipo.damage_relations.double_damage_to.length; i++){
                     infoDano.dobroDoDano = dadosTipo.damage_relations.double_damage_to[i]
                     danosPokemon.push(infoDano.dobroDoDano)
@@ -107,56 +89,25 @@ var pokemonsSalvos = fs.readFileSync('Desafios/Pokeapi/pokemons.json')
                 for(var j = 0; j < dadosTipo.damage_relations.half_damage_to.length; j++){
                     infoDano.metadeDoDano = dadosTipo.damage_relations.half_damage_to[j]
                     danosPokemon2.push(infoDano.metadeDoDano)
-                    // infoDano.nenhumDano = dadosTipo.damage_relations.no_damage_to[i]
+                    
                 }
                 for(var k = 0; k < dadosTipo.damage_relations.no_damage_to.length; k++){
                     infoDano.nenhumDano = dadosTipo.damage_relations.no_damage_to[k]
                     danosPokemon3.push(infoDano.nenhumDano)
-                //     // infoDano.nenhumDano = dadosTipo.damage_relations.no_damage_to[i]
+                
+                }
+                for(var n =0; n < dadosTipo.pokemon.length; n++ ){
+                    infoDano.mesmoTipo = dadosTipo.pokemon[n]
+                    tiposPokemon.push(infoDano.mesmoTipo)
                 }
                
                 console.log('Dobro do dano: ', danosPokemon)
                 console.log('Metade do dano: ', danosPokemon2)
                 console.log('Nenhum dano: ', danosPokemon3)
-                
-                
-            //     dadosHab.effect_entries.forEach((element) => {
-            //     infoHabilidade.efeito.push(element.effect)
-            //     habilidade.push(infoHabilidade)
-            //     console.log(habilidade)
-        
-            // })
+                console.log('Pokemons mesmo tipo: ', tiposPokemon)
+
         }).catch((error) => {
             console.log(error)
         })
         })
 })
-
-
-    
-// } //fechamento função
-
-
-// .then(trataRespostaApi)
-
-
-// .then((resposta) => {
-//         let dataHab = resposta.data
-//         var pokemonsSalvos = fs.readFileSync('Desafios/Pokeapi/pokemons.json')
-//         var pokemons = JSON.parse(pokemonsSalvos)    
-//         var pokemonSerializado = JSON.stringify(pokemons)
-//         fs.writeFileSync('Desafios/Pokeapi/pokemons.json', pokemonSerializado)
-       
-//         pokemons.forEach((pokemon) => {
-//             pokemon.habilidade.forEach((habilidade) => {
-//                 axios.get(`https://pokeapi.co/api/v2/ability/${habilidade}`)
-//                 var infoHabilidade = {descricao: ''}
-                
-//                 infoHabilidade.descricao = dataHab.effect_entries[i]
-//                 pokemons.push(infoHabilidade)
-//                 })
-//         })
-        
-// }).catch((error) => {
-//     console.log(error)
-// })
