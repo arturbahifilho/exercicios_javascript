416f30dd68e2c94846c86b7458ec0bb5e5836f5b
Artur Bahi Filho <artureng7@gmail.com>
2020-06-29 01:55:44 -0300

    tarefa intermediaria inicio

--- Desafios/Pokeapi/pokeapi.js
+++ Desafios/Pokeapi/pokeapi.js
@@ -1,12 +1,12 @@
-
-
-    let axios = require('axios')
+let axios = require('axios')
 const rs = require('readline-sync')
 var fs = require('fs')
 
 var filtro = rs.question('Digite o nome ou numero de um pokemon: ').toLocaleLowerCase()
 
-function trataRespostaApi(resp) {
+axios.get(`https://pokeapi.co/api/v2/pokemon/${filtro}`)
+// function trataRespostaApi(resp) {
+    .then((resp) => {
     let dados = resp.data
     var infoPokemon = {nome: '', id: '',tipo: [], habilidade: []}
     var pokemons = []
@@ -20,26 +20,22 @@ function trataRespostaApi(resp) {
             infoPokemon.habilidade.push(element.ability.name)
         })
 
-        // var pokemonsSalvos = fs.readFileSync('Desafios/Pokedex/pokemons.json')
+        // axios.get(`https://pokeapi.co/api/v2/ability/${infoPokemon.habilidade}`)
+        // var pokemonsSalvos = fs.readFileSync('Desafios/Pokeapi/pokemons.json')
         // var pokemons = JSON.parse(pokemonsSalvos)
     
         // var pokemonSerializado = JSON.stringify(pokemons)
         // fs.writeFileSync('Desafios/Pokedex/pokemons.json', pokemonSerializado)
-        // pokemons.habilidade.forEach((habilidade) => {
-        //     axios.get(`https://pokeapi.co/api/v2/ability${habilidade}`)
-        //         .then((resposta) => {
-        //             let dadosHabilidade = resposta.data
-        //             var infoHabilidade = {descricao: ''}
-        //             infoHabilidade.descricao = dadosHabilidade.effect_entries[1]
-        //             pokemons.push(infoHabilidade)
-        //             // console.log(infoHabilidade)
-        //         }).catch((error) => {
-        //             console.log(error)
-        //         })
-        //     })
-   
+        // var infoHabilidade = {efeito: []}
+        // var habilidade = []
+        // dados.forms.forEach((element) => {
+        // infoHabilidade.efeito.push(element.name)
+        // habilidade.push(infoHabilidade)
+        // console.log(habilidade)
+        // })
+
     pokemons.push(infoPokemon)
-    console.log(infoPokemon)
+    console.log(pokemons)
 
     var pergunta = rs.question('Voce deseja salvar o pokemon? Sim / Nao: ')
         if(pergunta == 'Sim' || pergunta == 'sim' || pergunta == 's'){
@@ -54,31 +50,65 @@ function trataRespostaApi(resp) {
         } else {
             console.log('Digite apenas Sim ou Nao')
         }
-} //fechamento function trataRespostaApi
-
-// function detalhesHabilidade(resp) {
     
+
+    }).catch((error) => {
+        console.log(error)
+    }) 
+
+    var pokemonsSalvos = fs.readFileSync('Desafios/Pokeapi/pokemons.json')
+    var pokemons = JSON.parse(pokemonsSalvos)
+    // var pokemonSerializado = JSON.stringify(pokemons)
+    // fs.writeFileSync('Desafios/Pokedex/pokemons.json', pokemonSerializado)
+    pokemons.forEach((pokemon) => {
+    pokemon.habilidade.forEach((habilidade) => {
+        axios.get(`https://pokeapi.co/api/v2/ability/${habilidade}`)
+        .then((resposta) => {
+            let dadosHab = resposta.data
+            var infoHabilidade = {efeito: []}
+                var habilidade = []
+                for(var i = 1; i < pokemon.habilidade.length; i++){
+                    infoHabilidade.efeito = dadosHab.effect_entries[i]
+                    habilidade.push(infoHabilidade)
+                    console.log(habilidade)
+                }
+            //     dadosHab.effect_entries.forEach((element) => {
+            //     infoHabilidade.efeito.push(element.effect)
+            //     habilidade.push(infoHabilidade)
+            //     console.log(habilidade)
+        
+            // })
+        }).catch((error) => {
+            console.log(error)
+        })
+        })
+})
+
+
     
-//     pokemons.habilidade.forEach((habilidade) => {
-//         axios.get(`https://pokeapi.co/api/v2/ability/${habilidade}`)
-//         var infoHabilidade = {descricao: ''}
-//         infoHabilidade.descricao = dados.effect_entries[1]
-//         pokemons.push(infoHabilidade)
-//     })
-        // } //fechamento função detalhesHabilidade
+// } //fechamento função
 
 
-axios.get(`https://pokeapi.co/api/v2/pokemon/${filtro}`)
-.then(trataRespostaApi)
-.catch((error) => {
-    console.log(error)
-}) 
+// .then(trataRespostaApi)
 
-// var dadosParaHabilidade = []
 
+// .then((resposta) => {
+//         let dataHab = resposta.data
+//         var pokemonsSalvos = fs.readFileSync('Desafios/Pokeapi/pokemons.json')
+//         var pokemons = JSON.parse(pokemonsSalvos)    
+//         var pokemonSerializado = JSON.stringify(pokemons)
+//         fs.writeFileSync('Desafios/Pokeapi/pokemons.json', pokemonSerializado)
        
-
-   
-
-   
-
+//         pokemons.forEach((pokemon) => {
+//             pokemon.habilidade.forEach((habilidade) => {
+//                 axios.get(`https://pokeapi.co/api/v2/ability/${habilidade}`)
+//                 var infoHabilidade = {descricao: ''}
+                
+//                 infoHabilidade.descricao = dataHab.effect_entries[i]
+//                 pokemons.push(infoHabilidade)
+//                 })
+//         })
+        
+// }).catch((error) => {
+//     console.log(error)
+// })
