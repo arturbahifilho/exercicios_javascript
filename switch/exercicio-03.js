49be68531eadf6c16eba7a8e91b1620821d55e93
Artur Bahi Filho <artureng7@gmail.com>
2020-07-02 11:30:09 -0300

    função que verifica se hoje é dia de colocar o lixo pra fora. O lixeiro passa nos dias 2, 4 e 6 (number)

---
+++ switch/exercicio-03.js
@@ -0,0 +1,19 @@
+// verificar se hoje é dia de colocar o lixo pra fora. O lixeiro passa nos dias 2, 4 e 6 (number).
+
+const diaDaSemana = 5;
+ 
+switch(diaDaSemana) {
+    
+    case 2:  
+        case 4:
+          case 6:
+            console.log("Coloque o lixo pra fora");
+            
+        break;
+                 
+          default:
+            console.log("Hoje não é o dia");
+            break;
+          }
+
+  
