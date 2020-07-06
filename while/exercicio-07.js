bcf3bb25a7a6f092b085217a9cc6a70d119a70db
Artur Bahi Filho <artureng7@gmail.com>
2020-07-06 16:10:35 -0300

    imprimir os valores da variável SO

---
+++ while/exercicio-07.js
@@ -0,0 +1,10 @@
+//imprimir os valores da variável SO
+
+let SO = ["Windows", "Linux", "Mac OS"];
+let contador = 0;
+ 
+while (contador< SO.length) {
+  console.log(SO[contador]);
+ 
+contador++;
+}
