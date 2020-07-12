1fd798add1562630e423a57c098a081b4f47d5d3
Artur Bahi Filho <artureng7@gmail.com>
2020-07-12 20:53:03 -0300

    retornar o número correspondente ao dia da semana e o dia do mês

---
+++ javascript-avancado/data-02.js
@@ -0,0 +1,9 @@
+// Complete o código para retornar na respectiva ordem os itens a seguir. O retorno deve ser do tipo number:
+
+// - O número correspondente ao dia da semana.
+// - O dia do mês.
+
+const hoje = new Date();
+ 
+console.log( hoje.getDay());
+console.log( hoje.getMonth());
