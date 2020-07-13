63cdd0c461c1e7e19975827ff32075d3bc410fdf
Artur Bahi Filho <artureng7@gmail.com>
2020-07-12 21:28:52 -0300

    retornar a seção da string a partir da palavra 'fatiar'

---
+++ javascript-avancado/slice.js
@@ -0,0 +1,8 @@
+//retornar a seção da string a partir da palavra 'fatiar'
+
+"'fatiar' uma string ou array e recuperar parte dos seus elementos"
+
+const titulo = "permite 'fatiar' uma string ou array e recuperar parte dos seus elementos";
+const novoTitulo = titulo.slice(8);
+ 
+console.log(novoTitulo);
