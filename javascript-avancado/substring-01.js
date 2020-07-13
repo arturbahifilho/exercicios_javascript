6fd0b6311361e3be6051aaa7965eb56536173c86
Artur Bahi Filho <artureng7@gmail.com>
2020-07-12 21:33:45 -0300

    imprimir apenas a palavra 'Date' da variável

---
+++ javascript-avancado/substring-01.js
@@ -0,0 +1,5 @@
+// Utilize o método 'substring' para imprimir apenas a palavra 'Date' da variável abaixo:
+
+const titulo = "JavaScript: Date, Arrays e String";
+ 
+console.log(titulo.substring(12,16) );
