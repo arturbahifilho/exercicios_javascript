d99124c55f6498cf28e60e704a3bd6d859ebdc0d
Artur Bahi Filho <artureng7@gmail.com>
2020-07-12 21:17:15 -0300

    percorrer a array e imprimir apenas os nomes da filial

---
+++ javascript-avancado/map-03.js
@@ -0,0 +1,15 @@
+//percorrer a array e imprimir apenas os nomes da filial
+
+const lojas = [
+    { nomeFilial : "Rio de Janeiro", faturamento : 20000},
+    { nomeFilial : "Paraná", faturamento : 40000},
+    { nomeFilial : "São Paulo", faturamento : 30000},
+  ];
+    
+  const nomesLojas = lojas.map(function(loja, indice){
+
+    return loja.nomeFilial
+  ;
+  });
+   
+  console.log(nomesLojas);
