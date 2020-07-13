8ac6e8c2e7ffac76e9f3fcfab429c698b173d1bd
Artur Bahi Filho <artureng7@gmail.com>
2020-07-12 21:00:54 -0300

    imprimir o número correspondente ao mês e o ano

---
+++ javascript-avancado/data-03.js
@@ -0,0 +1,10 @@
+//  Crie a variável 'hoje' com a data atual.
+//  Imprima o número correspondente ao mês
+//  Imprima o ano.
+
+const hoje = new Date()
+ 
+console.log( hoje.getMonth()+ 1);
+console.log( hoje.getFullYear() );
+
+
